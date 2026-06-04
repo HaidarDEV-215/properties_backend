0a683201a48c25f4d753affa97aa6c572e273265
Haidar Shawish <haidarshawishnl@gmail.com>
2026-06-04 15:04:36 +0300

    fix bugs in data limit in add property to check createdAt value and use tos image cleaner

--- middlewares/checkDailyPostLimit.js
+++ middlewares/checkDailyPostLimit.js
@@ -1,22 +1,16 @@
 const appError = require("../utils/appError.js");
 const httpStatus = require("../utils/HTTP.status.text.js");
 const Propertie = require("../models/property.model.js");
+const {rejectedPropertyImagesCleaner} = require("../helperFunctions/tosImageCleaner.js");
 
 const checkDailyPostLimit = async (req,res,next)=>{
     const userId = req.currentUser.id;
-    const properties = await Propertie.find({owner:userId});
-    if(properties.length >=10){
+    const todaysProperties = await Propertie.find({owner:userId,createdAt:{$gte:new Date().setHours(0,0,0,0),$lte:new Date().setHours(23,59,59,999)}});
+    if(todaysProperties.length >=10){
         const error = appError.create("Daily post limit reached. You can only post up to 10 properties per day.",403,httpStatus.FAIL);
         if(req.files && req.files.length>0){
-                    req.files.forEach(file => {
-                        //fs.unlink(`uploads/properties${file.filename}`,(err)=>{
-                        fs.unlink(file.path,(err)=>{
-                            const error = appError.create(`error while deleting property images`,500,httpStatusText.FAIL);
-                            return next(error);  
-                        })
-                        //console.log("file deleted")
-                    });
-                }
+            await rejectedPropertyImagesCleaner(req,res,next);
+        }
         return next(error);
     }
     next();
